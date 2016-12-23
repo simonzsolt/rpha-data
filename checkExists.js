aa0a21ed8b0f616620dca3ca893744c7c23aafba
Simon Zsolt <simon.zsolt@mail.com>
2016-12-23 11:55:52 +0100

    TODO: convert keys for author and others having: s* keys.

--- checkExists.js
+++ checkExists.js
@@ -1,6 +1,7 @@
-var exists = db.verse.find({ v : { $exists : true } }).count();
-var doesNotExist = db.verse.find({ v : { $exists : false } }).count();
+var exists = db.verse.find({ v12 : { $exists : true } }).count();
+var doesNotExist = db.verse.find({ v12 : { $exists : false } }).count();
 var total = db.verse.find().count();
 var unaccounted = total - exists - doesNotExist;
 
-print( exists + ' exists' + ' AND ' + doesNotExist + ' does not ' + 'OF ' + total + ' || ' +  unaccounted + ' unaccounted');
+print( exists + ' exists' + ' AND ' + doesNotExist + ' does not ' + 
+	'OF ' + total + ' || ' +  unaccounted + ' unaccounted BY checkExists.js');
--- renameField.js
+++ renameField.js
@@ -3,10 +3,24 @@ db = conn.getDB("rpha");
 
 db.verse.updateMany( { v : { $exists : true } }, { $rename : { 'v' : 'NEW_NAME' } } );
 
-db.verse.find({ v5 : { $exists : true } }).forEach(function(verse){
-	if (!Array.isArray(verse.v5)) {
-		
-		db.verse.update({ v5 : { $exists : true } }, { $rename : { v5.s1 : "author.surname" } });
-		
-	} // if Array
-});
+
+// IF ARRAY OF EMBEDED DOCUMENTS
+
+var tempArr = [];
+db.verse.find({ author : { $exists : true } }).forEach(function(verse){
+	if ( Array.isArray(verse.author) ) {   	    
+	  	for ( i=verse.author.length-1; i>=0; i-- ) {
+	  	  	tempArr.push(
+	  	  		{ 
+	  	  			'surname' : verse.author[i].s1, 
+	  	  			'forename' : verse.author[i].s2, 
+	  	  			'note' : verse.author[i].s3
+	  	  			// '_id' : verse._id,
+	  	  			// 'i' : i
+	  	  		}); // push
+	  	} // for
+	  	db.verse.update({ '_id' : verse._id }, { $set : { 'originalTitle' : tempArr } });
+	  	db.verse.update({ '_id' : verse._id }, { $unset : { 'v12' : '' } });
+	  	tempArr = [];
+	} // if
+}); // forEach
