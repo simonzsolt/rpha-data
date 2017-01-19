441a2f9844221de692e2d45879b6a944d77f3988
Simon Zsolt <simon.zsolt@mail.com>
2017-01-19 17:47:55 +0100

    update sources in verse docs

--- sourceIds.js
+++ sourceIds.js
@@ -1,38 +1,36 @@
-var tempPrint = [];
-var tempWritten = [];
+db.verse.find({
+    "printedSources": {
+        $exists: true
+    }
+}).forEach(function(verse) {
+    for (i = verse.printedSources.length - 1; i >= 0; i--) {
+        verse.printedSources[i].type = "print";
+        verse.printedSources[i].sourceId = verse.id + "-" + verse.printedSources[i].bookId;
+        db.verse.update({
+            "_id": verse._id
+        }, {
+            $push: {
+                "source": verse.printedSources[i]
+            }
+        })
+    }
+})
 
 db.verse.find({
-        "source": {
-            $exists: true
-        }
-    }).forEach(function(verse) {
-            if (verse.source.print) {
-                for (i = verse.source.print.length - 1; i >= 0; i--) {
-                    //print(verse.id + "-" + verse.source.print[i].bookId )
-                    verse.source.print[i].sourceId = verse.id + "-" + verse.source.print[i].bookId;
-                    tempPrint.push(verse.source.print[i]);
-                }
-                db.verse.update({
-                    "_id": verse._id
-                }, {
-                    $set: {
-                        "source.print": tempPrint
-                    }
-                })
-                tempPrint = [];
-
-            } else if (verse.source.written) {
-                for (j = verse.source.written.length - 1; j >= 0; j--) {
-                    //print(verse.id + "-" + verse.source.written[j].bookId )
-                    verse.source.written[j].sourceId = verse.id + "-" + verse.source.written[j].bookId;
-                    tempWritten.push(verse.source.written[j]);
-                }
-                db.verse.update({
-                    "_id": verse._id
-                }, {
-                    $set: {
-                        "source.written": tempWritten
-                    }
-                })
-                tempWritten = [];
+    "writtenSources": {
+        $exists: true
+    }
+}).forEach(function(verse) {
+    for (i = verse.writtenSources.length - 1; i >= 0; i--) {
+        verse.writtenSources[i].type = "written";
+        verse.writtenSources[i].sourceId = verse.id + "-" + verse.writtenSources[i].bookId;
+        db.verse.update({
+            "_id": verse._id
+        }, {
+            $push: {
+                "source": verse.writtenSources[i]
             }
+        })
+
+    }
+})
